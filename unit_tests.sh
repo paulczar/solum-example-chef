93b4041ea70ba221a7af50f2f7213d52cc730dfb
Paul Czarkowski <paul@paulcz.net>
2014-07-24 14:16:40 -0500

    Update unit_tests.sh

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,9 +1,9 @@
-  #!/bin/bash
+#!/bin/bash
 
-  export USE_SYSTEM_GECODE=1
-  bundle install
-  [[ $? == 0 ]] || exit 1
-  bundle exec berks install
-  [[ $? == 0 ]] || exit 1
-  bundle exec rake style spec
-  [[ $? == 0 ]] || exit 1
+export USE_SYSTEM_GECODE=1
+bundle install
+[[ $? == 0 ]] || exit 1
+bundle exec berks install
+[[ $? == 0 ]] || exit 1
+bundle exec rake style spec
+[[ $? == 0 ]] || exit 1
